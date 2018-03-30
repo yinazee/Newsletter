a08fdddb8cc535835765579b315b1cba8f527078
Yina Zhu <yinagit@gmail.com>
2018-03-30 04:55:07 +0000

    created article and scraper object

--- lib/newsletter/newsletter.rb
+++ lib/newsletter/newsletter.rb
@@ -1,4 +1,25 @@
 class Newsletter
 
-  attr_accessor :articles, :issue_number, :issue_date
+  attr_accessor :article, :issue_number, :issue_date
+
+  def initialize
+    @articles = []
+  end
+
+  def articles
+    @articles.dup.freeze
+  end
+
+  def add_article(article)
+      if !article.is_a?(Article) && !article.title.empty?
+      #the && part is optional, you can define article however you want
+      raise InvalidType, "must be an Article"
+    else
+      @articles << article
+    end
+
+    def first_article_url
+      self.articles.first.url
+    end
+
 end
